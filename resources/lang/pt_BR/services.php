a5d776493539e11a531a93f1432855bdd7f62fdc
HigorJSilva <higor.junio@hotmail.com>
2021-11-18 14:28:16 -0300

    Correção para exclusão de registro
    Também exibe mensagem correta caso não encontre registro

--- resources/lang/pt_BR/services.php
+++ resources/lang/pt_BR/services.php
@@ -12,5 +12,6 @@ return [
     'erros' => [
             'erro_interno' => 'Houve um erro interno no servidor',
             'no_results'=> 'Nenhum resultado encontrado para esse Id',
+            'nao_autorizado'=> 'Usuário não autorizado para essa ação',
     ]
 ];
