b6d2a9a0e531dbdbdadf54f59eb6ed287243a6fd
HigorJSilva <higor.junio@hotmail.com>
2021-11-18 13:24:02 -0300

    Adição de mensagens de validação em português

---
+++ resources/lang/pt_BR/services.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mensagens padrão
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'erros' => [
+            'erro_interno' => 'Houve um erro interno no servidor',
+            'no_results'=> 'Nenhum resultado encontrado para esse Id',
+    ]
+];
