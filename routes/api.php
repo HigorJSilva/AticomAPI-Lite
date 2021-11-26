4d5446c1dcea194bc73b29fc3c1a89f86d0845dd
HigorJSilva <higor.junio@hotmail.com>
2021-11-26 13:41:46 -0300

    função para obtenção do progresso do aluno

--- routes/api.php
+++ routes/api.php
@@ -26,9 +26,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('atividades', AtividadesController::class);
 Route::apiResource('referencias', ReferenciasController::class);
 Route::apiResource('certificados', CertificadosController::class);
+
+Route::name('modalidades.')->prefix('modalidades/')->group(function () {
+    Route::get('get-progresso', [ModalidadesController::class, 'getProgresso']);
+});
+
 Route::apiResource('modalidades', ModalidadesController::class)->except(['store', 'delete']);
 
-//TODO: dentro do group da autenticação
 Route::apiResource('users', UsersController::class)->except('store');
-//TODO:fora da autenticação
 Route::apiResource('users', UsersController::class)->only('store');
